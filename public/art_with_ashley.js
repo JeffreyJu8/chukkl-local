01aec274c90cf46812486bc2ac00430d4e4a1c41
Jeffrey Ju <jeffreyju1899@gmail.com>
2024-09-07 16:23:46 -0700

    retore video item

--- public/art_with_ashley.js
+++ public/art_with_ashley.js
@@ -1,3 +1,6 @@
+let previousVideoContainer = null;
+let previousVideoThumbnail = '';   
+
 function playVideo(videoUrl) {
     const regex = /vimeo\.com\/(\d+)/;
     const match = videoUrl.match(regex);
@@ -9,12 +12,24 @@ function playVideo(videoUrl) {
 
         var videoContainer = event.currentTarget.querySelector('div[id^="video-container"]');
 
+        // Reset the previously clicked video container
+        if (previousVideoContainer && previousVideoContainer !== videoContainer) {
+            previousVideoContainer.innerHTML = previousVideoThumbnail;
+        }
+
+        // Save the current container's default content
+        previousVideoThumbnail = videoContainer.innerHTML;
+
+        // Replace the current video container content with the iframe
         videoContainer.innerHTML = `
             <iframe src="${embedUrl}"
                     width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen
                     style="margin-right: 20px;">
             </iframe>
         `;
+
+        // Update the previousVideoContainer to the current one
+        previousVideoContainer = videoContainer;
     } else {
         console.error('Invalid Vimeo URL format');
     }
